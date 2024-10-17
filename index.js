5154d4955177d1f427491cc03ffb58d645256179
JustinpThomasson <Foreveraendeavour@gmail.com>
2024-10-17 14:33:29 -0400

    added CSS template, HTML template, json template  and a bunch of js constants.

---
+++ index.js
@@ -0,0 +1,25 @@
+//Constants
+const carousel = document.querySelector("#astro-events")
+const focusEvent = document.querySelector("#astro-event-detail")
+const focusImage = document.querySelector("#detail-image")
+const focusName = document.querySelector("#detail-name")
+const focusLocation = document.querySelector("#detail-location")
+const focusDate = document.querySelector("#date-display")
+const focusViews = document.querySelector("#view-display")
+const focusComment  = document.querySelector("#comment-display")
+const newEvent  = document.querySelector("#new-event")
+const newName  = document.querySelector("#new-name")
+const newLocation  = document.querySelector("#new-location")
+const newDate  = document.querySelector("#new-date")
+const newImage  = document.querySelector("#new-image")
+const newViews  = document.querySelector("#new-views")
+const newComment  = document.querySelector("#new-comment")
+const submitButton = document.querySelector("#submit-button")
+// const  = document.querySelector("#")
+// const  = document.querySelector("#")
+
+//Functions
+
+//Event listeners (3distinct event listeners)
+
+//Fetch 
