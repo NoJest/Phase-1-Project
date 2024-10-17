9908b7e5a5027a58da633e1404f9b665f6d6d21c
JustinpThomasson <Foreveraendeavour@gmail.com>
2024-10-17 16:26:02 -0400

    added a bunch of JS and CS, got the basic version of my website running and just need to add more functionality and event listeners.

--- index.js
+++ index.js
@@ -19,7 +19,53 @@ const submitButton = document.querySelector("#submit-button")
 // const  = document.querySelector("#")
 
 //Functions
+function addToCarousel (data) {
+    const eventPicture = document.createElement ('img')
+    eventPicture.src = data.image
+    carousel.append(eventPicture)
+    eventPicture.addEventListener('click',() => handleClick(data))
+    }
+
+
+    async function eventSubmission(event) {
+        event.preventDefault()
+        const newEventImage = newImage.value
+        const newEventName = newName.value
+        const newEventLocation = newLocation.value
+        const newEventViews = newViews.value
+        const newEventComment = newComment.value
+        
+        const response = await fetch('http://localhost:3000/astro-events', {
+          method: "POST",
+          headers: { "Content-Type": 'application/json' },
+          body: JSON.stringify( { name: newEventName, location: newEventLocation, image: newEventImage, views: newEventViews, comment:newEventComment  } )
+        })
+        
+        const newEventInput = await response.json()
+        addToCarousel(newEventInput)
+        
+        newEvent.reset()
+      }
 
 //Event listeners (3distinct event listeners)
+async function handleClick (data) {
+    focusImage.src = data.image
+    focusName.textContent = data.name
+    focusLocation.textContent = data.newLocation
+    focusDate.textContent = data.date
+    focusViews.textContent = data.views
+    focusComment.textContent = data.comments
+   };
+
 
 //Fetch 
+
+async function displayEvents () {
+    const response = await fetch('http://localhost:3000/astro-events')
+    const data = await response.json()
+    data.forEach(addToCarousel)
+  
+    handleClick(data[0])
+}
+
+displayEvents()
